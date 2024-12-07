1beba401f4a67d4ba429315ce4183bd2445925ba
camilaValentin <camilavalentin.1997@gmail.com>
2024-12-06 21:53:05 -0300

    inicio

--- 2 Pre-Entrega/JS/main.js
+++ 2 Pre-Entrega/JS/main.js
@@ -1,7 +1,7 @@
 /* SISTEMA DE MANEJO DE RESERVAS INTERNO DE UN RESTAURANTE */
 
 let reservas;
-if (localStorage.getItem("reservas")) { 
+if (localStorage.getItem("reservas")) {
     reservas = JSON.parse(localStorage.getItem("reservas"));
 } else {
     reservas = [];
@@ -10,9 +10,10 @@ let opcion;
 let confirmacion;
 let nombre;
 let indice;
+let formulario = document.getElementById("formulario");
 
-class Reserva{
-    constructor (nombre, cantidadPersonas, cantidadInfante, fecha, numeroContacto){
+class Reserva {
+    constructor(nombre, cantidadPersonas, cantidadInfante, fecha, numeroContacto) {
         this.nombre = nombre;
         this.cantidadPersonas = cantidadPersonas;
         this.cantidadInfante = cantidadInfante;
@@ -20,173 +21,30 @@ class Reserva{
         this.numeroContacto = numeroContacto;
     }
 }
-function menuOpciones() {
-    let opcion = parseInt(prompt("MILO RESTO\n\nMENÚ DE OPCIONES\n1.Agendar una reserva\n2.Consultar reservas\n3.Modificar una reserva\n4.Cancelar una reserva\n0.Salir"));
-    return opcion;
-}
 
-function busquedaConNombre(nombre) {
-    for (let reserva of reservas) {
-        if (reserva.nombre === nombre) {
-            return reserva;
-        }
+function agregarReserva(e) {
+    e.preventDefault();
+    const nombre = document.getElementById("nombre").value;
+    const numeroContacto = document.getElementById("contacto").value;
+    const fecha = document.getElementById("fecha").value;
+    const cantidadPersonas = document.getElementById("personas").value;
+    const cantidadInfante = document.getElementById("infantes").value;
+    if (nombre === "" || cantidadPersonas === "" || cantidadInfante === "" || fecha === "" || numeroContacto === "") {
+        alert("Todos los campos son obligatorios");
+        return;
     }
-    return -1;
+    let reserva = new Reserva(nombre, cantidadPersonas, cantidadInfante, fecha, numeroContacto);
+    reservas.push(reserva);
+    localStorage.setItem("reservas", JSON.stringify(reservas));
+    alert("Reserva realizada con éxito");
+    formulario.style.display = "none";
+    formulario.reset()
 }
 
-function nuevaReserva() {
-    confirmacion = true
-    while (confirmacion) {
-        let nombre = prompt("MILO RESTO\n\nIngrese el nombre de la persona");
-        let numeroContacto = parseInt(prompt("Ingrese el número de contacto"));
-        let fecha = prompt("MILO RESTO\n\nIngrese la fecha (formato dd/mm/aaa)");
-        let cantidadPersonas = prompt("MILO RESTO\n\nIngrese la cantidad de personas");
-        let infante = confirm("MILO RESTO\n\nDentro de la cantidad de personas, hay niños que necesiten silla aparte?");
-        if (infante) {
-            let cantidadInfante = parseInt(prompt("MILO RESTO\n\nIngrese la cantidad de sillas de niños necesarias"));
-            while (cantidadInfante > cantidadPersonas){
-                cantidadInfante = parseInt(prompt("MILO RESTO\n\nLa cantidad de niños no puede ser mayor a la cantidad de personas ingresadas ("+cantidadPersonas+")\nIngrese la cantidad de sillas de niños necesarias"))
-            }
-            let nuevaReserva = new Reserva (nombre, numeroContacto, fecha, cantidadPersonas, cantidadInfante);
-            reservas.push(nuevaReserva);
-            alert("MILO RESTO\n\nSe ingresó una reserva a nombre de " + nombre + " para " + cantidadPersonas + " personas (" + cantidadInfante + " niños), el dia " + fecha + ". Contacto: " + numeroContacto);
-        } else {
-            let cantidadInfante = 0;
-            let nuevaReserva = new Reserva (nombre, numeroContacto, fecha, cantidadPersonas, cantidadInfante);
-            reservas.push(nuevaReserva);
-            alert("MILO RESTO\n\nSe ingresó una reserva a nombre de " + nombre + " para " + cantidadPersonas + " personas el dia " + fecha + ". Contacto: " + numeroContacto);
-        }
-        localStorage.setItem("reservas",JSON.stringify(reservas));
-        confirmacion = confirm("MILO RESTO\n\nDesea ingresar otra reserva?");
-    }
-}
+const btnNuevaReserva = document.getElementById("btn-nueva-reserva");
+btnNuevaReserva.addEventListener("click", () => {
+    formulario.style.display = "block";
+});
 
-function mostrarReservas() {
-    if (reservas.length === 0) {
-        alert("No hay reservas")
-    } else {
-        for (const reserva of reservas) {
-            if (reserva.cantidadInfante > 0) {
-                alert("MILO RESTO\n\nFecha: " + reserva.fecha + "\nNombre: " + reserva.nombre + "\nCantidad de personas: " + reserva.cantidadPersonas + ", niños: " + reserva.cantidadInfante + "\nNúmero de contacto: " + reserva.numeroContacto)
-            } else {
-                alert("MILO RESTO\n\nFecha: " + reserva.fecha + "\nNombre: " + reserva.nombre + "\nCantidad de personas: " + reserva.cantidadPersonas + "\nNúmero de contacto: " + reserva.numeroContacto)
-            }
-        }
-    }
-}
+formulario.addEventListener("submit", agregarReserva);
 
-function modificarReserva() {
-    if (reservas.length === 0) {
-        alert("No hay reservas")
-    } else {
-        confirmacion = true;
-        nombre = prompt("MILO RESTO\n\nIngrese el nombre de la reserva a modificar");
-        let reserva = busquedaConNombre(nombre);
-        if (reserva === -1) {
-            alert("MILO RESTO\n\n" + nombre + " no se encuentra en la lista de reservas");
-        } else {
-            while (confirmacion) {
-                opcion = parseInt(prompt("MILO RESTO\n\nQue deseas cambiar?\n1.Fecha\n2.Nombre\n3.Cantidad de personas\n4.Cantidad de niños\n5.Número de contacto"));
-                switch (opcion) {
-                    case 1:
-                        modificarFecha(reserva);
-                        break;
-                    case 2:
-                        modificarNombre(reserva);
-                        break;
-                    case 3:
-                        modificarCantidad(reserva);
-                        break;
-                    case 4:
-                        modificarInfantes(reserva);
-                        break;
-                    case 5:
-                        modificarContacto(reserva);
-                        break;
-                    default:
-                        alert("MILO RESTO\n\nOpción incorrecta")
-                        break;
-                }
-                mensajeModificacion(reserva);
-                localStorage.setItem("reservas",JSON.stringify(reservas));
-                confirmacion = confirm("MILO RESTO\n\nDeseas hacer otra modificación a esta reserva?")
-            }
-        }
-    }
-
-}
-
-function modificarFecha(reserva) {
-    let fechaNueva = prompt("MILO RESTO\n\nFecha actual: " + reserva.fecha + "\nIngrese la fecha nueva (formato dd/mm/aaa)");
-    reserva.fecha = fechaNueva;
-
-}
-
-function modificarCantidad(reserva) {
-    let cantidadNueva = parseInt(prompt("MILO RESTO\n\nCantidad actual: " + reserva.cantidadPersonas + "\nIngrese la cantidad de personas nueva"));
-    reserva.cantidadPersonas = cantidadNueva
-}
-
-function modificarNombre(reserva) {
-    let nombreNuevo = prompt("MILO RESTO\n\nNombre actual: " + reserva.nombre + "\nIngrese el nombre nuevo");
-    reserva.nombre = nombreNuevo;
-}
-
-function modificarInfantes(reserva) {
-    let cantidadNueva = parseInt(prompt("MILO RESTO\n\nCantidad actual: " + reserva.cantidadInfante + "\nIngrese la cantidad nueva de niños"));
-    reserva.cantidadInfante = cantidadNueva;
-}
-
-function modificarContacto(reserva) {
-    let numeroNuevo = prompt("MILO RESTO\n\nNúmero actual: " + reserva.numeroContacto + "\nIngrese el número nuevo");
-    reserva.numeroContacto = numeroNuevo;
-}
-
-function mensajeModificacion(reserva) {
-    if (reserva.cantidadInfante <= 0) {
-        alert("MILO RESTO\n\nSe modificó correctamente la reserva\nFecha: " + reserva.fecha + "\nNombre: " + reserva.nombre + "\nCantidad de personas: " + reserva.cantidadPersonas + "\nNúmero de contacto: " + reserva.numeroContacto);
-    } else {
-        alert("MILO RESTO\n\nSe modificó correctamente la reserva\nFecha: " + reserva.fecha + "\nNombre: " + reserva.nombre + "\nCantidad de personas: " + reserva.cantidadPersonas + ", niños: " + reserva.cantidadInfante + "\nNúmero de contacto: " + reserva.numeroContacto);
-    }
-}
-
-function eliminarReserva() {
-    if (reservas.length === 0) {
-        alert("No hay reservas")
-    } else {
-        nombre = prompt("MILO RESTO\n\nIngrese el nombre de la reserva a cancelar");
-        let reserva = busquedaConNombre(nombre);
-        if (reserva === -1) {
-            alert("MILO RESTO\n\n" + nombre + " no se encuentra en la lista de reservas");
-        } else {
-            indice = reservas.indexOf(reserva);
-            reservas.splice(indice, 1);
-            alert("MILO RESTO\n\nSe canceló la reserva a nombre de " + nombre);
-            localStorage.setItem("reservas",JSON.stringify(reservas));
-        }
-    }
-}
-
-while (opcion !== 0) {
-    opcion = menuOpciones();
-    switch (opcion) {
-        case 0:
-            alert("MILO RESTO\n\nPrograma terminado")
-            break;
-        case 1:
-            nuevaReserva();
-            break;
-        case 2:
-            mostrarReservas();
-            break;
-        case 3:
-            modificarReserva();
-            break;
-        case 4:
-            eliminarReserva();
-            break;
-        default:
-            alert("MILO RESTO\n\nOpción incorrecta")
-            break;
-    }
-}
